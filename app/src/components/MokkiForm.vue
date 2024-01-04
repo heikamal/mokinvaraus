82e27754aacd8a80e389c2699b79dc1d816e3078
Heikki Malkavaara <heimal@student.uef.fi>
2024-01-04 16:39:46 +0200

    added input to the form

---
+++ app/src/components/MokkiForm.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      form: {
+      name: '',
+      },
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>Lisää uusi mökki</h1>
+  <section class="form">
+    <label class="label">Nimi</label>
+    <div class="control">
+      <input v-model="form.name" class="input" type="text" placeholder="Mökin nimi">
+    </div>
+  </section>
+</template>
