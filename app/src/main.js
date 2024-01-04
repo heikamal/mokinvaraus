05c7cbe786eed747068f4c76ef7acfbd195b27d4
Heikki Malkavaara <heimal@student.uef.fi>
2024-01-04 16:41:38 +0200

    path for cabin form

--- app/src/main.js
+++ app/src/main.js
@@ -5,11 +5,13 @@ import App from './App.vue'
 import Home from './components/Home.vue'
 import Mokit from './components/Mokit.vue'
 import Mokki from './components/Mokki.vue'
+import MokkiForm from './components/MokkiForm.vue'
 
 const routes = [
-  { path: '/', component: Home},
-  { path: '/cabins', component: Mokit},
-  { path: '/cabins/:id', component: Mokki}
+  { path: '/', component: Home },
+  { path: '/cabins', component: Mokit },
+  { path: '/cabins/:id', component: Mokki },
+  { path: '/cabins/new', component: MokkiForm }
 ]
 
 const router = createRouter({
