0270d57f4118954d13b4219dd52451d1765c2d68
Heikki Malkavaara <heimal@student.uef.fi>
2024-01-04 16:00:11 +0200

    created component to show a single cabin

---
+++ app/src/components/Mokki.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return{
+      listItems: [],
+    }
+  },
+  methods: {
+    async getData() {
+      const res = await fetch("http://localhost:8080/api/cabins/"+ this.$route.params.id)
+      const finalRes = await res.json();
+      this.listItems = finalRes;
+      console.log(finalRes)
+      console.log(this.$route.params.id)
+    }
+  },
+  created() {
+    this.getData()
+    this.$watch(
+      () => this.$route.params,
+      (toParams, previousParams) => {
+        this.getData()
+      }
+    )
+  }
+}
+</script>
+
+<template>
+  <h1>Pöö</h1>
+</template>
